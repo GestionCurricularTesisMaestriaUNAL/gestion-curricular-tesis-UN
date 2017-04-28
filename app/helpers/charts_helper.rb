28dacf4fd1d44aabfd3089b7e802f3d29db5499a
Degly Pava <dspavap@unal.edu.co>
2017-04-28 02:48:56 -0500

    Charts done

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,7 +1,7 @@
 module ChartsHelper
   def users_count
-    bar_chart @users.group(:firstname).count, height: '300px', library: {
-      title: {text: 'Usuarios creados', x: -20},
+    bar_chart ({"Jurados" => @countJury , "Estudiantes" => @countStudent , "Profesores" => @countTeacher , "Administradores" => @countAdmin , "Usuarios MultiRol" => (@rolesCreados - @totalnuser.number)}) , height: '300px', width:'50%', library: {
+      title: {text: 'Usuarios creados', x: 0},
       yAxis: {
          allowDecimals: false,
          title: {
@@ -14,5 +14,43 @@ module ChartsHelper
          }
       }
     }
-  end
 end
+
+ def grading_count
+    bar_chart ({"Sin asignar estado" => @countnoStatus , "Calificado" => @statuscalificado , "Calificando" => @statuscalificando , "Sin calificar" => @statussincalificar}) , height: '300px', width:'50%', library: {
+      title: {text: 'Usuarios creados', x: -20},
+      colors: ['red', 'orange', 'green', 'blue', 'purple', 'brown'],      yAxis: {
+         allowDecimals: false,
+         title: {
+             text: 'Contador de usuarios'
+         }
+      },
+      xAxis: {
+         title: {
+             text: 'Usuarios'
+         }
+      }
+    }
+  end
+
+ def juries_counts
+    pie_chart ({"Juries" => @countJury , "Roles" => @rolesCreados}) , height: '300px', width: '50%', library: {
+      title: {text: 'Usuarios creados', x: -20},
+      colors: ['red', 'orange', 'green', 'blue', 'purple', 'brown'],      yAxis: {
+         allowDecimals: false,
+         title: {
+             text: 'Contador de usuarios'
+         }
+      },
+      xAxis: {
+         title: {
+             text: 'Usuarios'
+         }
+      }
+    }
+  end
+
+
+
+end
+
