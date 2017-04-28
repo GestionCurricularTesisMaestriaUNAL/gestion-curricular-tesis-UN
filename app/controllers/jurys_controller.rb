df7b4417f7afee45750efd341e3534be22eb7e68
Sebastian Morales <sebastian_vem@hotmail.com>
2017-04-28 00:47:52 -0500

    some changes

--- app/controllers/jurys_controller.rb
+++ app/controllers/jurys_controller.rb
@@ -1,6 +1,12 @@
 class JurysController < ApplicationController
+  
   def index
     @gradeworks = Gradework.all
+    @juries = User.users_jury
+  end
+
+  def new
+    @gradework = Gradework.new
   end
   
   def home
