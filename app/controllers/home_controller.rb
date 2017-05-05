00180909321770d9a9e57c77f3d0867b51f71f0e
Helmer Avendaño <haavendanov@unal.edu.co>
2017-05-05 00:58:03 -0500

    Route to jurys

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,7 +30,7 @@ class HomeController < ApplicationController
     protected
     def jury_home
       # ...
-      render '/jurys/home'
+      render '/jurys/index'
     end
 
     def student_home
