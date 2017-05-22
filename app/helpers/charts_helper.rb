ed9b5aecf702e6d4779ff9c22d238a59f184839f
Degly Pava <dspavap@unal.edu.co>
2017-05-22 04:40:34 -0500

    Charts

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,6 +1,6 @@
 module ChartsHelper
   def users_count
-    bar_chart ({"Jurados" => @countJury , "Estudiantes" => @countStudent , "Profesores" => @countTeacher , "Administradores" => @countAdmin , "Usuarios MultiRol" => (@rolesCreados - @totalnuser.number)}) , height: '300px', width:'70%', library: {
+    bar_chart ({"Jurados" => @countJury , "Estudiantes" => @countStudent , "Profesores" => @countTeacher , "Administradores" => @countAdmin , "Usuarios MultiRol" => (@rolesCreados - @totalnuser.number)}) , height: '300px', width:'100%', library: {
       title: {text: 'Usuarios creados con rol', x: 0},
       yAxis: {
          allowDecimals: false,
@@ -16,7 +16,7 @@ module ChartsHelper
     }
 end
 def userspie_counts
-   pie_chart ({"Jurados" => @countJury , "Estudiantes" => @countStudent , "Profesores" => @countTeacher , "Administradores" => @countAdmin}) , height: '300px', width: '50%', library: {
+   pie_chart ({"Jurados" => @countJury , "Estudiantes" => @countStudent , "Profesores" => @countTeacher , "Administradores" => @countAdmin}) , height: '300px', width: '100%', library: {
      title: {text: 'Roles asignados', x: 0},
      colors: ['red', 'orange', 'green', 'blue', 'purple', 'brown'],
      yAxis: {
@@ -33,7 +33,7 @@ def userspie_counts
    }
  end
  def usersrolpie_counts
-    pie_chart ({"Usuarios con rol" => (@totalnuser.number - @countnoRol) , "Usuarios sin rol" => @countnoRol}) , height: '300px', width: '50%', library: {
+    pie_chart ({"Usuarios con rol" => (@totalnuser.number - @countnoRol) , "Usuarios sin rol" => @countnoRol}) , height: '300px', width: '100%', library: {
       title: {text: 'Rol de usuarios', x: 0},
       colors: ['red', 'orange', 'green', 'blue', 'purple', 'brown'],      yAxis: {
          allowDecimals: false,
@@ -50,7 +50,7 @@ def userspie_counts
   end
 
  def grading_count
-    column_chart ({"Sin asignar estado" => @countnoStatus , "Calificado" => @statuscalificado , "Calificando" => @statuscalificando , "Sin calificar" => @statussincalificar}) , height: '300px', width:'70%', library: {
+    column_chart ({"Sin asignar estado" => @countnoStatus , "Calificado" => @statuscalificado , "Calificando" => @statuscalificando , "Sin calificar" => @statussincalificar}) , height: '300px', width:'100%', library: {
       title: {text: 'Estado de calificación tesis', x: -20},
       colors: ['red', 'orange', 'green', 'blue', 'purple', 'brown'],      yAxis: {
          allowDecimals: false,
@@ -66,7 +66,7 @@ def userspie_counts
     }
   end
   def tesisjur_count
-    bar_chart (@jurados.each) , height: '300px', width:'50%', library: {
+    bar_chart (@jurados.each) , height: '300px', width:'100%', library: {
       title: {text: 'Jurados vs tesis asignadas', x: 0},
       yAxis: {
          allowDecimals: false,
@@ -82,7 +82,7 @@ def userspie_counts
     }
   end
  def juries_counts
-    pie_chart ({"Jurados" => @countJury , "Roles" => @rolesCreados}) , height: '300px', width: '50%', library: {
+    pie_chart ({"Jurados" => @countJury , "Roles" => @rolesCreados}) , height: '300px', width: '100%', library: {
       title: {text: 'Jurados creados', x: -20},
       colors: ['red', 'orange', 'green', 'blue', 'purple', 'brown'],      yAxis: {
          allowDecimals: false,
@@ -98,7 +98,7 @@ def userspie_counts
     }
   end
   def dategrade_count
-    bar_chart ({"01-04-2017 - 15-04-2017" => @dategrade1, "16-04-2017 - 28-04-2017" => @dategrade2} ) , height: '300px', width:'70%', library: {
+    bar_chart ({"01-04-2017 - 15-04-2017" => @dategrade1, "16-04-2017 - 28-04-2017" => @dategrade2} ) , height: '300px', width:'100%', library: {
       title: {text: 'Tesis creadas en el tiempo', x: 0},
       yAxis: {
          allowDecimals: false,
